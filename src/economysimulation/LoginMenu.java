891bb825d7b38715e4291725db3cbfdfe1a3e70b
Max Home <MC170171@reigate.ac.uk>
2018-05-03 16:25:02 +0100

    Added seperate class for username log in.

--- src/economysimulation/LoginMenu.java
+++ src/economysimulation/LoginMenu.java
@@ -49,7 +49,7 @@ public class LoginMenu extends javax.swing.JPanel {
             }
 
             public void execute() {
-                begin.setEnabled(!"".equals(entername.getText()) && !GHOST_TEXT.equals(entername.getText()));
+                QLogIn.begin.setEnabled(!"".equals(QLogIn.entername.getText()) && !GHOST_TEXT.equals(QLogIn.entername.getText()));
             }
         });
         
@@ -57,8 +57,10 @@ public class LoginMenu extends javax.swing.JPanel {
     
     public LoginMenu() {
         initComponents();
-        checkTextFields(entername);
-        Methods.addGhostText(entername, GHOST_TEXT);
+        Methods.addToFrontPanel(right, new QLogIn(), false);
+        
+        checkTextFields(QLogIn.entername);
+        Methods.addGhostText(QLogIn.entername, GHOST_TEXT);
         timerStart();   
     }
 
@@ -67,13 +69,6 @@ public class LoginMenu extends javax.swing.JPanel {
     private void initComponents() {
 
         right = new javax.swing.JPanel();
-        jSeparator1 = new javax.swing.JSeparator();
-        logo = new javax.swing.JLabel();
-        jSeparator2 = new javax.swing.JSeparator();
-        jLabel1 = new javax.swing.JLabel();
-        entername = new javax.swing.JTextField();
-        jSeparator3 = new javax.swing.JSeparator();
-        begin = new javax.swing.JButton();
         picHold = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(204, 204, 204));
@@ -81,69 +76,15 @@ public class LoginMenu extends javax.swing.JPanel {
 
         right.setBackground(new java.awt.Color(255, 255, 255));
 
-        jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
-
-        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/economysimulation/resources/logos/logo-gif.gif"))); // NOI18N
-
-        jSeparator2.setOrientation(javax.swing.SwingConstants.VERTICAL);
-
-        jLabel1.setFont(new java.awt.Font("Agency FB", 1, 48)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(204, 0, 0));
-        jLabel1.setText("Username");
-
-        entername.setForeground(new java.awt.Color(153, 153, 153));
-        entername.setText("Enter username here");
-        entername.setBorder(null);
-
-        begin.setBackground(new java.awt.Color(255, 255, 255));
-        begin.setFont(new java.awt.Font("Agency FB", 1, 36)); // NOI18N
-        begin.setForeground(new java.awt.Color(204, 0, 0));
-        begin.setMnemonic('\r');
-        begin.setText("Get Started");
-        begin.setEnabled(false);
-        begin.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                beginActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout rightLayout = new javax.swing.GroupLayout(right);
         right.setLayout(rightLayout);
         rightLayout.setHorizontalGroup(
             rightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, rightLayout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(19, 19, 19)
-                .addGroup(rightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jSeparator3)
-                    .addComponent(entername, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(logo, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(begin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(28, 28, 28))
+            .addGap(0, 600, Short.MAX_VALUE)
         );
         rightLayout.setVerticalGroup(
             rightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, rightLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(rightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(rightLayout.createSequentialGroup()
-                        .addComponent(logo, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(entername, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(begin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(rightLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 700, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 700, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(45, 45, 45))
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
         picHold.setIcon(new javax.swing.ImageIcon(getClass().getResource("/economysimulation/resources/background/back1.png"))); // NOI18N
@@ -164,24 +105,8 @@ public class LoginMenu extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void beginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_beginActionPerformed
-        Methods.username = Methods.generateRandomUsername(entername.getText());
-        try {
-            MainFrame.addToMainFrame(new IntroPage());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }//GEN-LAST:event_beginActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton begin;
-    private javax.swing.JTextField entername;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JSeparator jSeparator2;
-    private javax.swing.JSeparator jSeparator3;
-    private javax.swing.JLabel logo;
     private javax.swing.JLabel picHold;
     private javax.swing.JPanel right;
     // End of variables declaration//GEN-END:variables
