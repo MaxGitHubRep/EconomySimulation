d1f4fb08e9b328b3bc754242d45521ec3cc48e61
Max College <mc170171@reigate.ac.uk>
2018-06-05 10:24:56 +0100

    Renamed Price level variable to CPI.

--- src/economysimulation/classes/algorithms/Component.java
+++ src/economysimulation/classes/algorithms/Component.java
@@ -8,7 +8,7 @@ public class Component {
     
     public static double
             INTEREST_RATE = 0.5, CORP_TAX = 0.5, CONS_TAX = 0.5,
-            UNEMPLOYMENT = 4.2, REAL_GDP, PRICE_LEVEL = 1, MPC;
+            UNEMPLOYMENT = 4.2, REAL_GDP, CPI = 1, MPC;
     public static int 
             CONSUMPTION, INVESTMENT, EXPORTS, IMPORTS,
             DISPOSABLE_INCOME, CONS_BORROWING, AUTO_CONS,
