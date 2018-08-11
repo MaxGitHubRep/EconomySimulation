b1cb73d1c996bcdbe32b4675daefd6926d9b205f
Max Home <MC170171@reigate.ac.uk>
2018-08-11 16:30:45 +0100

    Class with indexes of sector values.

---
+++ src/economysimulation/classes/algorithms/Sector.java
@@ -0,0 +1,19 @@
+package economysimulation.classes.algorithms;
+
+/**
+ *
+ * @author Max Carter
+ */
+public class Sector {
+
+    public static int 
+            NHS = 0,
+            EDUCATION = 1,
+            TRANSPORT = 2,
+            FOOD = 3,
+            INFRASTRUCTURE = 4,
+            DEFENCE = 5,
+            SCIENCE = 6,
+            BENEFITS = 7;
+
+}
