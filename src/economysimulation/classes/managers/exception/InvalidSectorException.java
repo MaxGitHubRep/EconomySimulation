3fbbbe06f24ef8a9552e00255686d7298cdf5a5c
Max Home <MC170171@reigate.ac.uk>
2018-08-11 16:30:26 +0100

    New exception to handle an invalid requested sector variable.

---
+++ src/economysimulation/classes/managers/exception/InvalidSectorException.java
@@ -0,0 +1,27 @@
+package economysimulation.classes.managers.exception;
+
+import economysimulation.classes.algorithms.Component;
+
+/**
+ *
+ * @author Max Carter
+ */
+public class InvalidSectorException extends Exception {
+    
+    /**
+    * Sends exception when an invalid sector id is requested.
+    */
+    public InvalidSectorException() {
+        super();
+    }
+    
+    /**
+    * Sends exception when an invalid sector id is requested.
+    * 
+    * @param id the invalid sector specified
+    */
+    public InvalidSectorException(int id) {
+        super("Invalid sector requested: " + id + ". Specified value must be between 0 and " + Component.BUDGET_VARS.length);
+    }
+    
+}
