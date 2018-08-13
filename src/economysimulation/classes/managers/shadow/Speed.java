0610da3558a4211d3e15a708297a4b97951cf45d
Max Home <MC170171@reigate.ac.uk>
2018-08-13 09:45:41 +0100

    Renamed class to speed.

---
+++ src/economysimulation/classes/managers/shadow/Speed.java
@@ -0,0 +1,16 @@
+package economysimulation.classes.managers.shadow;
+
+/**
+ *
+ * @author Max Carter
+ */
+public class Speed {
+    
+    public static final int
+            VERY_FAST = 1,
+            FAST = 3,
+            MEDIUM = 5,
+            SLOW = 8,
+            VERY_SLOW = 10;
+    
+}
