a4d63af56cab19535ab0533fbbc21aa1340a7492
Max Home <MC170171@reigate.ac.uk>
2018-08-18 16:03:43 +0100

    Improved consumer savings mechanism.

--- src/economysimulation/classes/economy/Component.java
+++ src/economysimulation/classes/economy/Component.java
@@ -1,6 +1,9 @@
 package economysimulation.classes.economy;
 
+import economysimulation.classes.managers.exception.InvalidPanelSizeException;
 import economysimulation.classes.managers.exception.InvalidSectorException;
+import economysimulation.classes.managers.popup.hint.HintManager;
+import economysimulation.classes.managers.popup.hint.Hints;
 import java.util.ArrayList;
 
 /**
@@ -17,7 +20,7 @@ public class Component {
             D_INCOME, TOTAL_SAVINGS, 
             CONSUMPTION, INVESTMENT, EXPORTS, IMPORTS, WORK_HOURS_PER_DAY = 8,
             TAXATION, ANNUAL_BUDGET = 250, FIRM_PROFITS,
-            TOTAL_CORP_PROFITS = 50, TOTAL_INVESTMENT, TOTAL_CONSUMPTION, TOTAL_TAX,
+            TOTAL_CORP_PROFITS, TOTAL_INVESTMENT, TOTAL_CONSUMPTION, TOTAL_TAX,
             TOTAL_CORP_TAX, TOTAL_INCOME_TAX, QUARTER_CORP_TAX, QUARTER_INCOME_TAX,
             YEARLY_INCOME_TAX, YEARLY_CORP_TAX, 
             COST_OF_PRODUCTION, WAGES, RESOURCE_COST, DISPOSABLE_INCOME,
@@ -80,7 +83,7 @@ public class Component {
         return confidence;
     }
 
-    public static void calcComp() throws InvalidSectorException {
+    public static void calcComp() throws InvalidSectorException, InvalidPanelSizeException {
 
         IMPORTS = 0;
         RESOURCE_COST = IMPORTS;
@@ -103,9 +106,17 @@ public class Component {
         CORP_CONFIDENCE = getPublicSpendingTotal(true) > ANNUAL_BUDGET ? ANNUAL_BUDGET / getPublicSpendingTotal(true) : 1;
         CONS_CONFIDENCE = getConsConfidence();
         
+        if (D_INCOME == 0 && TOTAL_SAVINGS >= 0.1) {
+            TOTAL_SAVINGS-=0.1;
+            D_INCOME+=0.1;
+        } else if (TOTAL_SAVINGS < 0.1 && D_INCOME == 0) {
+            HintManager.createNewHint(Hints.CONSUMERS_OUT_OF_MONEY);
+        }
+        
         MPC = ((100 - INTEREST_RATE)/100) * CONS_CONFIDENCE;
-        CONSUMPTION = MPC * ( D_INCOME + (CONS_INJECTION > 0 ? CONS_INJECTION : 0) + 0.5);
-        SAVINGS = (1 - MPC) * ( INCOME + (CONS_INJECTION > 0 ? CONS_INJECTION : 0));
+        CONSUMPTION = MPC * ( D_INCOME + CONS_INJECTION + 0.4 * (!TAX_BREAK[1] ? 1-(INCOME_TAX/100) : 1));
+        SAVINGS = (1 - MPC) * ( D_INCOME + CONS_INJECTION);
+        
         if (CONS_INJECTION > 0) CONS_INJECTION = 0;
 
         INVESTMENT = (FIRM_PROFITS - COST_OF_PRODUCTION) > 0 ? (FIRM_PROFITS - COST_OF_PRODUCTION) * CORP_CONFIDENCE : 0;
