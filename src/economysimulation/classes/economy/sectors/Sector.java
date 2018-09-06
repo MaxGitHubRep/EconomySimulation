ef8b0931eb65fd2af62d23b670f1a64209fe5195
Max Home <MC170171@reigate.ac.uk>
2018-09-06 19:36:28 +0100

    Replaced 8 classes with one.

--- src/economysimulation/classes/economy/sectors/Sector.java
+++ src/economysimulation/classes/economy/sectors/Sector.java
@@ -9,17 +9,35 @@ import economysimulation.classes.economy.budget.BudgetSector;
 public class Sector {
     
     public static final BudgetSector
-            NHS = new NHS(0),
-            Education = new Education(1),
-            Housing = new Housing(2),
-            Food = new Food(3),
-            Infrastructure = new Infrastructure(4),
-            Defence = new Defence(5),
-            Science = new Science(6),
-            Benefits = new Benefits(7);
+            NHS = new SectorBase(0.004, 3, 0),
+            Education = new SectorBase(0.003, 0, 1.1),
+            Housing = new SectorBase(0.0025, 0, 1.05),
+            Food = new SectorBase(0.003, 1, 0),
+            Infrastructure = new SectorBase(0.003, 0, 0),
+            Defence = new SectorBase(0.001, 1, 0),
+            Science = new SectorBase(0.0015, 3, 0),
+            Benefits = new SectorBase(0.0015, 1, 1.02);
 
     public static BudgetSector[] SectorList  = new BudgetSector[]{
         NHS, Education, Housing, Food, Infrastructure, Defence, Science, Benefits
     };
+    
+    /**
+     * Returns the amount of sectors.
+     * @return The length of the list. 
+     */
+    public static int getSectorSize() {
+        return SectorList.length;
+    }
+    
+    /**
+     * Will return the sector at the {@code index} of the list.
+     * @param index The index of the list.
+     * @return The sector located at a specific index in the sector list.     
+     */
+    public static BudgetSector getSector(int index) {
+        if (index < 0 || index > getSectorSize()) throw new NullPointerException();
+        return SectorList[index];
+    }
     
 }
---
+++ src/economysimulation/classes/economy/sectors/SectorBase.java
@@ -0,0 +1,59 @@
+package economysimulation.classes.economy.sectors;
+
+import economysimulation.classes.economy.budget.BudgetSector;
+
+/**
+ *
+ * @author Max Carter
+ */
+public class SectorBase extends BudgetSector {
+       
+    public SectorBase(double standardOfLiving, int population, double wageMultiplier) {
+        super.standardOfLiving = standardOfLiving;
+        super.population = population;
+        super.wageMultiplier = wageMultiplier;
+    }
+
+    @Override
+    public int getSpending() {
+        return super.spending;
+    }
+    
+    @Override
+    public void addSpending(int value) {
+        super.spending += value;
+    }
+
+
+    @Override
+    public double getSpendingInfluence() {
+        return super.spendingInfluence;
+    }
+    
+    @Override
+    public void addSpendingInfluence(double value) {
+        super.spendingInfluence += value;
+    }
+
+    @Override
+    public void setSpendingInfluence(double value) {
+        super.spendingInfluence = value;
+    }
+
+    @Override
+    public double getStandardLivingInfluence() {
+        return super.standardOfLiving;
+    }
+
+    @Override
+    public int getPopulationInfluence() {
+        return super.population;
+    }
+
+    @Override
+    public double getWageInfluence() {
+        return super.wageMultiplier;
+    } 
+
+
+}
