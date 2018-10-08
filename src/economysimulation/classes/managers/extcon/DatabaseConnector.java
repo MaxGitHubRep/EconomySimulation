d5644f1106e9d18a86e6e97d86fda9634528e937
Max Home <MC170171@reigate.ac.uk>
2018-10-08 21:11:37 +0100

    Changed database connection and local data handler.

--- src/economysimulation/classes/managers/extcon/DatabaseConnector.java
+++ src/economysimulation/classes/managers/extcon/DatabaseConnector.java
@@ -10,7 +10,7 @@ import java.sql.Statement;
  *
  * @author Max Carter
  */
-public class DatabaseConnection {
+public class DatabaseConnector {
     
     protected final String
             HOST = "jdbc:mysql://rds-mxcrtr-db.cejdk9ogcqcy.eu-west-2.rds.amazonaws.com:3306/mxcrtr_db",
@@ -21,14 +21,28 @@ public class DatabaseConnection {
     protected Statement Statement;
     protected ResultSet ResultSet;
     
+    private boolean isOnline = false;
+    
     /**
      * Establishes connection to database.
      * @throws SQLException When a safe connection cannot be established.
      */
-    public DatabaseConnection() throws SQLException {
+    public DatabaseConnector() throws SQLException {
         Connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
         Statement = Connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 
+    }
+    
+    public boolean isConnected() {
+        return isOnline;
+    }
+    
+    public void toggleConnection() {
+        isOnline = !isOnline;
+    }
+    
+    public void setConnection(boolean state) {
+        isOnline = state;
     }
     
 }
--- src/economysimulation/classes/managers/extcon/UserData.java
+++ src/economysimulation/classes/managers/extcon/UserData.java
@@ -1,11 +1,12 @@
 package economysimulation.classes.managers.extcon;
 
-import static economysimulation.classes.global.Methods.DBConnection;
 import economysimulation.classes.managers.exception.UserDataOverflowException;
 import economysimulation.classes.managers.exception.UserDataUnderflowException;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import static economysimulation.classes.global.Methods.DBConnector;
 
 /**
  *
@@ -14,7 +15,7 @@ import java.util.List;
 public class UserData {
 
     List<String> listNames = new ArrayList();
-    List<Integer> listIDs = new ArrayList();
+    private int lastUserID = 0;
     
     /**
      * Deals with the user data.
@@ -28,14 +29,14 @@ public class UserData {
      */
     public void refresh() {
         listNames.clear();
-        listIDs.clear();
+        lastUserID = 0;
         
         try {
-            DBConnection.ResultSet = DBConnection.Statement.executeQuery("SELECT * FROM mxcrtr_db.Users");
+            DBConnector.ResultSet = DBConnector.Statement.executeQuery("SELECT * FROM mxcrtr_db.Users");
             
-            while (DBConnection.ResultSet.next()) {
-                listNames.add(DBConnection.ResultSet.getString("Name"));
-                listIDs.add(DBConnection.ResultSet.getInt("UserID"));
+            while (DBConnector.ResultSet.next()) {
+                listNames.add(DBConnector.ResultSet.getString("Name"));
+                lastUserID++;
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -51,21 +52,13 @@ public class UserData {
     }
     
     /**
-     * List of all the user IDs saved on the database.
-     * @return List of user IDs.
-     */
-    public  List<Integer> getUserIDList() {
-        return listIDs;
-    }
-    
-    /**
      * List of all the user names and IDs concatenated together using local data.
      * @return List of user names and IDs.
      */
     public List<String> getCompleteUserList() {
         List<String> listConcat = new ArrayList();
         for (int i = 0; i < listNames.size(); i++) {
-            listConcat.add(listNames.get(i) + "#" + listIDs.get(i));
+            listConcat.add(listNames.get(i) + "#" + new DecimalFormat("00000").format(i));
         }
         return listConcat;
     }
@@ -75,7 +68,7 @@ public class UserData {
      * @return Size of users list.
      */
     public int getUserCount() {
-        return listIDs.size();
+        return lastUserID;
     }
     
     /**
@@ -83,21 +76,21 @@ public class UserData {
      * @return Latest user ID.
      */
     public int getLatestUserID() {
-        if (listIDs.isEmpty()) {
+        if (lastUserID == 0) {
             try {
                 throw new UserDataUnderflowException();
             } catch (UserDataUnderflowException ex) {
                 ex.printStackTrace();
             }
         }
-        return listIDs.get(listIDs.size()-1);
+        return lastUserID;
     }
 
     /**
      * Gets the next available User ID that can be used.
      * @return Unique user ID.
      */
-    public int getAvailableUserID() {
+    public int getNextAvailableUserID() {
         if (this.getUserCount() >= 99999) {
             try {
                 throw new UserDataOverflowException();
@@ -106,7 +99,7 @@ public class UserData {
             }
         }
 
-        return (listIDs.isEmpty()) ? 0 : getLatestUserID()+1;
+        return lastUserID + 1;
 
     }
     
