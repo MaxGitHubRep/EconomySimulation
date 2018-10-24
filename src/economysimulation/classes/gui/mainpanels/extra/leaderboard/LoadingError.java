5e800cbfb7624c691aa0f52768ade5cec1894551
Max Home <MC170171@reigate.ac.uk>
2018-10-24 21:28:30 +0100

    Leader board exception handling.

---
+++ src/economysimulation/classes/gui/mainpanels/extra/leaderboard/LoadingError.java
@@ -0,0 +1,164 @@
+package economysimulation.classes.gui.mainpanels.extra.leaderboard;
+
+import static economysimulation.classes.global.Methods.LBDisplay;
+import static economysimulation.classes.global.Methods.ThemeManager;
+import economysimulation.classes.managers.theme.GraphicUpdater;
+import economysimulation.classes.managers.theme.ThemeUpdateEvent;
+import economysimulation.classes.managers.ui.Format;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Max Carter
+ */
+public class LoadingError extends javax.swing.JPanel implements ThemeUpdateEvent {
+
+    /**
+     * Creates new form LoadingError
+     */
+    public LoadingError(String message) {
+        initComponents();
+        errorMessage.setText(message);
+        Format.addButtonFormat(back1, color1);
+        ThemeManager.addThemeUpdateListener(this);
+    }
+    
+    @Override
+    public void updateThemeEvent(GraphicUpdater updater) {
+        updater.applyPanelThemes(new JPanel[]{ back1 }, new JPanel[]{ prop });
+        updater.applyTextThemes(new JLabel[]{ titleMain, reload }, new JLabel[]{ errorMessage });
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        titleMain = new javax.swing.JLabel();
+        prop = new javax.swing.JPanel();
+        errorMessage = new javax.swing.JLabel();
+        back1 = new javax.swing.JPanel();
+        reload = new javax.swing.JLabel();
+        color1 = new javax.swing.JPanel();
+
+        setOpaque(false);
+
+        titleMain.setFont(new java.awt.Font("Agency FB", 0, 48)); // NOI18N
+        titleMain.setForeground(new java.awt.Color(204, 0, 0));
+        titleMain.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        titleMain.setText("Error Loading the Leaderboard");
+
+        prop.setBackground(new java.awt.Color(204, 204, 204));
+
+        errorMessage.setFont(new java.awt.Font("Agency FB", 0, 24)); // NOI18N
+        errorMessage.setForeground(new java.awt.Color(255, 255, 255));
+        errorMessage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        errorMessage.setText("No games found.");
+
+        javax.swing.GroupLayout propLayout = new javax.swing.GroupLayout(prop);
+        prop.setLayout(propLayout);
+        propLayout.setHorizontalGroup(
+            propLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(propLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(errorMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 490, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        propLayout.setVerticalGroup(
+            propLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(propLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(errorMessage)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        back1.setBackground(new java.awt.Color(255, 255, 255));
+        back1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        reload.setFont(new java.awt.Font("Agency FB", 0, 24)); // NOI18N
+        reload.setForeground(new java.awt.Color(204, 0, 0));
+        reload.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        reload.setText("Reload");
+        reload.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        reload.setFocusable(false);
+        reload.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                reloadMouseClicked(evt);
+            }
+        });
+
+        color1.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout color1Layout = new javax.swing.GroupLayout(color1);
+        color1.setLayout(color1Layout);
+        color1Layout.setHorizontalGroup(
+            color1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 272, Short.MAX_VALUE)
+        );
+        color1Layout.setVerticalGroup(
+            color1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 25, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout back1Layout = new javax.swing.GroupLayout(back1);
+        back1.setLayout(back1Layout);
+        back1Layout.setHorizontalGroup(
+            back1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(color1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(reload, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        back1Layout.setVerticalGroup(
+            back1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(back1Layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(reload)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(color1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(titleMain, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(197, 197, 197)
+                        .addComponent(prop, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 195, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(314, 314, 314)
+                .addComponent(back1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(229, 229, 229)
+                .addComponent(titleMain)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(prop, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 386, Short.MAX_VALUE)
+                .addComponent(back1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void reloadMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_reloadMouseClicked
+        LBDisplay.configLeaderboard();
+    }//GEN-LAST:event_reloadMouseClicked
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel back1;
+    private javax.swing.JPanel color1;
+    private javax.swing.JLabel errorMessage;
+    private javax.swing.JPanel prop;
+    private javax.swing.JLabel reload;
+    private javax.swing.JLabel titleMain;
+    // End of variables declaration//GEN-END:variables
+}
