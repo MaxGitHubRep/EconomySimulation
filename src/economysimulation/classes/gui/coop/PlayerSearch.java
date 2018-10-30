02dc5414d15d627ab33564929e5eaab6882631c2
Max Home <MC170171@reigate.ac.uk>
2018-10-30 21:10:07 +0000

    Stared UI for player search.

--- src/economysimulation/classes/gui/coop/PlayerSearch.java
+++ src/economysimulation/classes/gui/coop/PlayerSearch.java
@@ -1,10 +1,14 @@
 package economysimulation.classes.gui.coop;
 
+import economysimulation.classes.managers.theme.GraphicUpdater;
+import economysimulation.classes.managers.theme.ThemeUpdateEvent;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Max Carter
  */
-public class PlayerSearch extends javax.swing.JPanel {
+public class PlayerSearch extends javax.swing.JPanel implements ThemeUpdateEvent {
 
     /**
      * Creates new form PlayerSearch
@@ -12,6 +16,12 @@ public class PlayerSearch extends javax.swing.JPanel {
     public PlayerSearch() {
         initComponents();
     }
+
+    @Override
+    public void updateThemeEvent(GraphicUpdater updater) {
+        updater.applyPanelThemes(new JPanel[]{ this }, null);
+    }
+
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -21,14 +31,13 @@ public class PlayerSearch extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1800, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 1000, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
