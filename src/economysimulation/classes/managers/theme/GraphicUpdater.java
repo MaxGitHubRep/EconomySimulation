a8f3a6ba73dabd2a034aa78a97a82c7aa0cb6354
Max College <MC170171@reigate.ac.uk>
2018-11-28 08:54:44 +0000

    Added support for radio buttons.

--- src/economysimulation/classes/managers/theme/GraphicUpdater.java
+++ src/economysimulation/classes/managers/theme/GraphicUpdater.java
@@ -3,6 +3,7 @@ package economysimulation.classes.managers.theme;
 import static economysimulation.classes.global.Methods.ThemeManager;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 /**
  *
@@ -10,6 +11,12 @@ import javax.swing.JPanel;
  */
 public class GraphicUpdater {
 
+    /**
+     * Changes the theme colour of the labels.
+     * 
+     * @param primaryTextLabels   List of primary labels.
+     * @param secondaryTextLabels List of secondary labels.
+     */
     public void applyTextThemes(JLabel[] primaryTextLabels, JLabel[] secondaryTextLabels) {
         if (primaryTextLabels != null) {
             for (JLabel text : primaryTextLabels) {
@@ -24,6 +31,12 @@ public class GraphicUpdater {
         }
     }
     
+    /**
+     * Changes the theme colour of the panels.
+     * 
+     * @param primaryBackPanels   List of primary panels.
+     * @param secondaryBackPanels List of secondary panels.
+     */
     public void applyPanelThemes(JPanel[] primaryBackPanels, JPanel[] secondaryBackPanels) {
         if (primaryBackPanels != null) {
             for (JPanel panel : primaryBackPanels) {
@@ -36,6 +49,27 @@ public class GraphicUpdater {
                 panel.setBackground(ThemeManager.Theme.getSecondaryColor());
             }
         }
+    }
+    
+    /**
+     * Changes the theme colour of the buttons.
+     * 
+     * @param primaryButtons   List of primary buttons.
+     * @param secondaryButtons List of secondary buttons.
+     */
+    public void applyRadioButtonThemes(JRadioButton[] primaryButtons, JRadioButton[] secondaryButtons) {
+        if (primaryButtons != null) {
+            for (JRadioButton button : primaryButtons) {
+                button.setForeground(ThemeManager.Theme.getPrimaryColor());
+            }
+        }
+        
+        if (secondaryButtons != null) {
+            for (JRadioButton button : secondaryButtons) {
+                button.setForeground(ThemeManager.Theme.getSecondaryColor());
+            }
+        }
+        
     }
      
 }
